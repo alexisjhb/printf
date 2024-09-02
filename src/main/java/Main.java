7688f128d6e19246ff00c00d60a1ff767dc22f2a
Alexis Hernández bolivar <169068450+alexisjhb@users.noreply.github.com>
2024-09-02 19:02:12 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "alexis";
+   int dia = 3;
+   int mes = 5;
+   int ano = 2008;
+   double pi = 3.14159;
+ System.out.printf(" o vaor de pi é: %2.f ",pi );
+ 
+    
+   }
+
+}
